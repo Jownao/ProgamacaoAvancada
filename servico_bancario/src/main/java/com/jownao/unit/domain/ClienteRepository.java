2620733ffe447ac449011cb2ad5198eb1fd17a80
Jownao <50759662+Jownao@users.noreply.github.com>
2021-05-30 16:40:20 -0300

    Exercício 6 - API REST - Bank Service

---
+++ servico_bancario/src/main/java/com/jownao/unit/domain/ClienteRepository.java
@@ -0,0 +1,16 @@
+package com.jownao.unit.domain;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Double> { // interface repository -> sptring data JPA
+
+    Optional<Cliente> findByNome(String nome); //nome completo
+    Optional<Cliente> findByNomeContaining(String nome);//partes do nome
+    Optional<Cliente> findByNomeContainingIgnoreCase(String nome);//ignorar caixa alta
+    Optional<Cliente> findByStartDateBetween();
+
+}

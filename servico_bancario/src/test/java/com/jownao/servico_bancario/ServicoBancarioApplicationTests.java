5f799a2ef1961de7bc2feec37dd727d1490ea38a
Jownao <50759662+Jownao@users.noreply.github.com>
2021-05-30 16:38:50 -0300

    -

---
+++ servico_bancario/src/test/java/com/jownao/servico_bancario/ServicoBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jownao.servico_bancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicoBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
